334cf70ed2f3bda18fda3ec159a6a1995d94b30e
Henry Steven Fuérez Morán <henry256@hotmail.es>
2023-07-14 12:51:11 -0500

    fix: token link

--- utils/emails/pass-recovery.js
+++ utils/emails/pass-recovery.js
@@ -5,7 +5,7 @@ exports.PASS_RECOVERY_EMAIL = (name, token) => {
     <p>Estimado(a) ${name},</p>
     <br>
     <p>Ha solicitado recuperar tu contraseña. Ingrese al siguiente enlace para poder restablecerla.</p>  
-    <p><a href="${process.env.WEB_URL}/reset-password/${token}">Restablecer</a></p>
+    <p><a href="${process.env.WEB_URL}/reset-password/?token=${token}">Restablecer</a></p>
     <p>Si no ha solicitado restablecer tu contraseña, puede ignorar este correo electrónico.</p>
     <br>
     <p>Saludos,</p>
