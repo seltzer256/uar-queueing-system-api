d55e89591af5e26e71674999e0637e3b8d8196ec
Henry Steven Fuérez Morán <henry256@hotmail.es>
2023-07-16 09:16:45 -0500

    fix: client schema

--- models/clientModel.js
+++ models/clientModel.js
@@ -3,11 +3,8 @@ const mongoose = require('mongoose');
 const clientSchema = new mongoose.Schema({
   email: {
     type: String,
-  },
-  documentId: {
-    type: String,
-    required: true,
     unique: true,
+    required: true,
   },
   firstName: String,
   lastName: String,
