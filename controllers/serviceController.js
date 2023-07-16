8a24e15120a7812f78d5153c9f21093ca4222f26
Henry Steven Fuérez Morán <henry256@hotmail.es>
2023-07-16 09:30:01 -0500

    fix: find active services

--- controllers/serviceController.js
+++ controllers/serviceController.js
@@ -4,7 +4,41 @@ const factory = require('./handlerFactory');
 exports.getAllServices = factory.getAll(Service);
 
 exports.getActiveServices = catchAsync(async (req, res, next) => {
-  const services = await Service.find({ active: true });
+  // const services = await Service.find({ active: true });
+  const services = await Service.aggregate([
+    {
+      $match: {
+        active: true,
+      },
+    },
+    {
+      $lookup: {
+        from: 'modules',
+        localField: '_id',
+        foreignField: 'services',
+        as: 'module',
+      },
+    },
+    {
+      $lookup: {
+        from: 'users',
+        localField: 'module.user',
+        foreignField: '_id',
+        as: 'users',
+      },
+    },
+    {
+      $project: {
+        module: 0,
+        users: {
+          password: 0,
+          __v: 0,
+          role: 0,
+        },
+      },
+    },
+  ]);
+
   res.status(200).json({
     status: 'success',
     results: services.length,
